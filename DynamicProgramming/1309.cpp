e60de096ffb8ff4feea91d8356317a9772e31a72
iamyaeeun <108451447+iamyaeeun@users.noreply.github.com>
2024-02-07 01:23:06 +0900

    [백준] 1309번 동물원

---
+++ DynamicProgramming/1309.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int dp[100001] = { 1,3 };
+
+int main() {
+	int N;
+	cin >> N;
+	for (int i = 2; i <= N; i++) {
+		dp[i] = (dp[i - 2] + dp[i - 1] * 2) % 9901;
+	}
+	cout << dp[N];
+
+	return 0;
+}
